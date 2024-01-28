601dbbb4aee0c6234dff509b9e548ede7dcaa186
sbx_user1051 <sbx_user1051@169.254.28.81>
2024-01-28 16:08:05 +0000

    Sun Jan 28 16:08:05 UTC 2024

--- src/main/kotlin/solutions/CHK/CheckoutSolution.kt
+++ src/main/kotlin/solutions/CHK/CheckoutSolution.kt
@@ -2,13 +2,6 @@ package solutions.CHK
 
 object CheckoutSolution {
 
-    private val pricing = mapOf(
-        "A" to 50,
-        "B" to 30,
-        "C" to 20,
-        "D" to 15
-    )
-
     fun checkout(skus: String): Int {
         val aCount = skus.filter { it == 'A' }
         val bCount = skus.filter { it == 'B' }
@@ -26,5 +19,6 @@ object CheckoutSolution {
         }
     }
 }
+
 
 
