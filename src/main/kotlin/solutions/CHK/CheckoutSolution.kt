69902d0097c2b325eacb41dc679fa521580b3b65
sbx_user1051 <sbx_user1051@169.254.28.81>
2024-01-28 16:01:54 +0000

    Sun Jan 28 16:01:54 UTC 2024

--- src/main/kotlin/solutions/CHK/CheckoutSolution.kt
+++ src/main/kotlin/solutions/CHK/CheckoutSolution.kt
@@ -1,7 +1,25 @@
 package solutions.CHK
 
 object CheckoutSolution {
+
+    private val pricing = mapOf(
+        "A" to 50,
+        "B" to 30,
+        "C" to 20,
+        "D" to 15
+    )
+
     fun checkout(skus: String): Int {
         TODO("Solution not implemented")
+    }
+
+    fun getPrice(sku: String, number: Int = 1): Int {
+        return when (sku) {
+            "A" -> 50
+            "B" -> 30
+            "C" -> 20
+            "D" -> 15
+            else -> throw IllegalArgumentException()
+        }
     }
 }
---
+++ src/test/kotlin/solutions/CHK/CheckoutSolutionTest.kt
@@ -0,0 +1,13 @@
+package solutions.CHK
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class CheckoutSolutionTest {
+
+    @Test
+    fun `Complex case`() {
+
+    }
+
+}
